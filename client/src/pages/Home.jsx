227a924514ab7e856bfff580b70903adc8482567
Himanshu Yadav <himanshuya1008@gmail.com>
2026-04-29 14:12:14 +0530

    Initial commit

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
+import AiTools from '../components/AiTools'
+import Testimonoial from '../components/Testimonoial'
+import Plans from '../components/Plans'
+import Footer from '../components/Footer'
+
+const Home = () => {
+   
+    
+  return (
+    <>
+     <Navbar/>
+     <Hero/>
+     <AiTools/>
+     <Testimonoial/>
+     <Plans/>
+     <Footer/>
+    </>
+  )
+}
+
+export default Home
